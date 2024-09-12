d81cc1c764af3bb820719cbf94462da329fd44d3
arthu <arthu@DESKTOP-1K8VP9G>
2024-09-11 21:33:54 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraRosen/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace CalculadoraRosen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSete = new Button();
+            btnOito = new Button();
+            btnNove = new Button();
+            btnQuatro = new Button();
+            btnCinco = new Button();
+            btnSeis = new Button();
+            btnUm = new Button();
+            btnDois = new Button();
+            btnTres = new Button();
+            btnZero = new Button();
+            btnVirgula = new Button();
+            btnBinario = new Button();
+            btnDiv = new Button();
+            btnMult = new Button();
+            btnSoma = new Button();
+            btnSub = new Button();
+            btnLimpar = new Button();
+            btnIgual = new Button();
+            btnSen = new Button();
+            btnCos = new Button();
+            btnTan = new Button();
+            btnFatorial = new Button();
+            txtHistorico = new TextBox();
+            btnLimparUm = new Button();
+            SuspendLayout();
+            // 
+            // btnSete
+            // 
+            btnSete.BackColor = Color.LightSkyBlue;
+            btnSete.Location = new Point(12, 73);
+            btnSete.Name = "btnSete";
+            btnSete.Size = new Size(55, 51);
+            btnSete.TabIndex = 0;
+            btnSete.Text = "7";
+            btnSete.UseVisualStyleBackColor = false;
+            btnSete.Click += btnSete_Click;
+            // 
+            // btnOito
+            // 
+            btnOito.BackColor = Color.LightSkyBlue;
+            btnOito.Location = new Point(73, 73);
+            btnOito.Name = "btnOito";
+            btnOito.Size = new Size(55, 51);
+            btnOito.TabIndex = 2;
+            btnOito.Text = "8";
+            btnOito.UseVisualStyleBackColor = false;
+            btnOito.Click += btnOito_Click;
+            // 
+            // btnNove
+            // 
+            btnNove.BackColor = Color.LightSkyBlue;
+            btnNove.Location = new Point(134, 73);
+            btnNove.Name = "btnNove";
+            btnNove.Size = new Size(55, 51);
+            btnNove.TabIndex = 3;
+            btnNove.Text = "9";
+            btnNove.UseVisualStyleBackColor = false;
+            btnNove.Click += btnNove_Click;
+            // 
+            // btnQuatro
+            // 
+            btnQuatro.BackColor = Color.LightSkyBlue;
+            btnQuatro.Location = new Point(12, 130);
+            btnQuatro.Name = "btnQuatro";
+            btnQuatro.Size = new Size(55, 51);
+            btnQuatro.TabIndex = 4;
+            btnQuatro.Text = "4";
+            btnQuatro.UseVisualStyleBackColor = false;
+            btnQuatro.Click += btnQuatro_Click;
+            // 
+            // btnCinco
+            // 
+            btnCinco.BackColor = Color.LightSkyBlue;
+            btnCinco.Location = new Point(73, 130);
+            btnCinco.Name = "btnCinco";
+            btnCinco.Size = new Size(55, 51);
+            btnCinco.TabIndex = 5;
+            btnCinco.Text = "5";
+            btnCinco.UseVisualStyleBackColor = false;
+            btnCinco.Click += btnCinco_Click;
+            // 
+            // btnSeis
+            // 
+            btnSeis.BackColor = Color.LightSkyBlue;
+            btnSeis.Location = new Point(134, 130);
+            btnSeis.Name = "btnSeis";
+            btnSeis.Size = new Size(55, 51);
+            btnSeis.TabIndex = 6;
+            btnSeis.Text = "6";
+            btnSeis.UseVisualStyleBackColor = false;
+            btnSeis.Click += btnSeis_Click;
+            // 
+            // btnUm
+            // 
+            btnUm.BackColor = Color.LightSkyBlue;
+            btnUm.Location = new Point(12, 187);
+            btnUm.Name = "btnUm";
+            btnUm.Size = new Size(55, 51);
+            btnUm.TabIndex = 7;
+            btnUm.Text = "1";
+            btnUm.UseVisualStyleBackColor = false;
+            btnUm.Click += btnUm_Click;
+            // 
+            // btnDois
+            // 
+            btnDois.BackColor = Color.LightSkyBlue;
+            btnDois.Location = new Point(73, 187);
+            btnDois.Name = "btnDois";
+            btnDois.Size = new Size(55, 51);
+            btnDois.TabIndex = 8;
+            btnDois.Text = "2";
+            btnDois.UseVisualStyleBackColor = false;
+            btnDois.Click += btnDois_Click;
+            // 
+            // btnTres
+            // 
+            btnTres.BackColor = Color.LightSkyBlue;
+            btnTres.Location = new Point(134, 187);
+            btnTres.Name = "btnTres";
+            btnTres.Size = new Size(55, 51);
+            btnTres.TabIndex = 9;
+            btnTres.Text = "3";
+            btnTres.UseVisualStyleBackColor = false;
+            btnTres.Click += btnTres_Click;
+            // 
+            // btnZero
+            // 
+            btnZero.BackColor = Color.LightSkyBlue;
+            btnZero.Location = new Point(12, 244);
+            btnZero.Name = "btnZero";
+            btnZero.Size = new Size(55, 51);
+            btnZero.TabIndex = 10;
+            btnZero.Text = "0";
+            btnZero.UseVisualStyleBackColor = false;
+            btnZero.Click += btnZero_Click;
+            // 
+            // btnVirgula
+            // 
+            btnVirgula.BackColor = Color.LightSkyBlue;
+            btnVirgula.ForeColor = SystemColors.ControlText;
+            btnVirgula.Location = new Point(73, 244);
+            btnVirgula.Name = "btnVirgula";
+            btnVirgula.Size = new Size(55, 51);
+            btnVirgula.TabIndex = 11;
+            btnVirgula.Text = ",";
+            btnVirgula.UseVisualStyleBackColor = false;
+            btnVirgula.Click += btnVirgula_Click;
+            // 
+            // btnBinario
+            // 
+            btnBinario.BackColor = Color.DeepSkyBlue;
+            btnBinario.ForeColor = SystemColors.Control;
+            btnBinario.Location = new Point(134, 244);
+            btnBinario.Name = "btnBinario";
+            btnBinario.Size = new Size(55, 51);
+            btnBinario.TabIndex = 12;
+            btnBinario.Text = "binário";
+            btnBinario.UseVisualStyleBackColor = false;
+            btnBinario.Click += btnBinario_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.BackColor = SystemColors.Desktop;
+            btnDiv.ForeColor = Color.DeepSkyBlue;
+            btnDiv.Location = new Point(195, 73);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(55, 51);
+            btnDiv.TabIndex = 13;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = false;
+            btnDiv.Click += btnDiv_Click;
+            // 
+            // btnMult
+            // 
+            btnMult.BackColor = SystemColors.Desktop;
+            btnMult.ForeColor = Color.DeepSkyBlue;
+            btnMult.Location = new Point(195, 130);
+            btnMult.Name = "btnMult";
+            btnMult.Size = new Size(55, 51);
+            btnMult.TabIndex = 14;
+            btnMult.Text = "x";
+            btnMult.UseVisualStyleBackColor = false;
+            btnMult.Click += btnMult_Click;
+            // 
+            // btnSoma
+            // 
+            btnSoma.BackColor = SystemColors.Desktop;
+            btnSoma.ForeColor = Color.DeepSkyBlue;
+            btnSoma.Location = new Point(195, 187);
+            btnSoma.Name = "btnSoma";
+            btnSoma.Size = new Size(55, 51);
+            btnSoma.TabIndex = 15;
+            btnSoma.Text = "+";
+            btnSoma.UseVisualStyleBackColor = false;
+            btnSoma.Click += btnSoma_Click;
+            // 
+            // btnSub
+            // 
+            btnSub.BackColor = SystemColors.Desktop;
+            btnSub.ForeColor = Color.DeepSkyBlue;
+            btnSub.Location = new Point(195, 244);
+            btnSub.Name = "btnSub";
+            btnSub.Size = new Size(55, 51);
+            btnSub.TabIndex = 16;
+            btnSub.Text = "-";
+            btnSub.UseVisualStyleBackColor = false;
+            btnSub.Click += btnSub_Click;
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.BackColor = SystemColors.Desktop;
+            btnLimpar.ForeColor = SystemColors.Control;
+            btnLimpar.Location = new Point(12, 358);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.Size = new Size(55, 51);
+            btnLimpar.TabIndex = 17;
+            btnLimpar.Text = "C";
+            btnLimpar.UseVisualStyleBackColor = false;
+            btnLimpar.Click += btnLimpar_Click;
+            // 
+            // btnIgual
+            // 
+            btnIgual.BackColor = SystemColors.Desktop;
+            btnIgual.ForeColor = Color.DeepSkyBlue;
+            btnIgual.Location = new Point(73, 358);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(116, 51);
+            btnIgual.TabIndex = 18;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = false;
+            btnIgual.Click += btnIgual_Click;
+            // 
+            // btnSen
+            // 
+            btnSen.BackColor = Color.DeepSkyBlue;
+            btnSen.ForeColor = SystemColors.Control;
+            btnSen.Location = new Point(12, 301);
+            btnSen.Name = "btnSen";
+            btnSen.Size = new Size(55, 51);
+            btnSen.TabIndex = 19;
+            btnSen.Text = "sen";
+            btnSen.UseVisualStyleBackColor = false;
+            btnSen.Click += btnSen_Click;
+            // 
+            // btnCos
+            // 
+            btnCos.BackColor = Color.DeepSkyBlue;
+            btnCos.ForeColor = SystemColors.Control;
+            btnCos.Location = new Point(73, 301);
+            btnCos.Name = "btnCos";
+            btnCos.Size = new Size(55, 51);
+            btnCos.TabIndex = 20;
+            btnCos.Text = "cos";
+            btnCos.UseVisualStyleBackColor = false;
+            btnCos.Click += btnCos_Click;
+            // 
+            // btnTan
+            // 
+            btnTan.BackColor = Color.DeepSkyBlue;
+            btnTan.ForeColor = SystemColors.Control;
+            btnTan.Location = new Point(134, 301);
+            btnTan.Name = "btnTan";
+            btnTan.Size = new Size(55, 51);
+            btnTan.TabIndex = 21;
+            btnTan.Text = "tan";
+            btnTan.UseVisualStyleBackColor = false;
+            btnTan.Click += btnTan_Click;
+            // 
+            // btnFatorial
+            // 
+            btnFatorial.BackColor = Color.DeepSkyBlue;
+            btnFatorial.ForeColor = SystemColors.Control;
+            btnFatorial.Location = new Point(195, 301);
+            btnFatorial.Name = "btnFatorial";
+            btnFatorial.Size = new Size(55, 51);
+            btnFatorial.TabIndex = 22;
+            btnFatorial.Text = "fatorial";
+            btnFatorial.UseVisualStyleBackColor = false;
+            btnFatorial.Click += btnFatorial_Click;
+            // 
+            // txtHistorico
+            // 
+            txtHistorico.Location = new Point(12, 27);
+            txtHistorico.Name = "txtHistorico";
+            txtHistorico.Size = new Size(238, 23);
+            txtHistorico.TabIndex = 23;
+            txtHistorico.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnLimparUm
+            // 
+            btnLimparUm.BackColor = SystemColors.Desktop;
+            btnLimparUm.ForeColor = SystemColors.Control;
+            btnLimparUm.Location = new Point(195, 358);
+            btnLimparUm.Name = "btnLimparUm";
+            btnLimparUm.Size = new Size(55, 51);
+            btnLimparUm.TabIndex = 24;
+            btnLimparUm.Text = "<-";
+            btnLimparUm.UseVisualStyleBackColor = false;
+            btnLimparUm.Click += btnLimparUm_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Captura_de_tela_2024_09_11_211405;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(262, 418);
+            Controls.Add(btnLimparUm);
+            Controls.Add(txtHistorico);
+            Controls.Add(btnFatorial);
+            Controls.Add(btnTan);
+            Controls.Add(btnCos);
+            Controls.Add(btnSen);
+            Controls.Add(btnIgual);
+            Controls.Add(btnLimpar);
+            Controls.Add(btnSub);
+            Controls.Add(btnSoma);
+            Controls.Add(btnMult);
+            Controls.Add(btnDiv);
+            Controls.Add(btnBinario);
+            Controls.Add(btnVirgula);
+            Controls.Add(btnZero);
+            Controls.Add(btnTres);
+            Controls.Add(btnDois);
+            Controls.Add(btnUm);
+            Controls.Add(btnSeis);
+            Controls.Add(btnCinco);
+            Controls.Add(btnQuatro);
+            Controls.Add(btnNove);
+            Controls.Add(btnOito);
+            Controls.Add(btnSete);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnSete;
+        private Button btnOito;
+        private Button btnNove;
+        private Button btnQuatro;
+        private Button btnCinco;
+        private Button btnSeis;
+        private Button btnUm;
+        private Button btnDois;
+        private Button btnTres;
+        private Button btnZero;
+        private Button btnVirgula;
+        private Button btnBinario;
+        private Button btnDiv;
+        private Button btnMult;
+        private Button btnSoma;
+        private Button btnSub;
+        private Button btnLimpar;
+        private Button btnIgual;
+        private Button btnSen;
+        private Button btnCos;
+        private Button btnTan;
+        private Button btnFatorial;
+        private TextBox txtHistorico;
+        private Button btnLimparUm;
+    }
+}
---
+++ CalculadoraRosen/Form1.cs
@@ -0,0 +1,406 @@
+using System.Configuration;
+using System.Linq;
+
+namespace CalculadoraRosen
+{
+
+    public partial class Form1 : Form
+    {
+        private const string vazio = "";
+        string valores;
+        string sinais = "";
+
+
+        public decimal Resultado { get; set; }
+        public decimal Valor { get; set; }
+
+        private Operacao OperacaoSelecionada { get; set; }
+
+        private enum Operacao
+        {
+            Div,
+            Mult,
+            Soma,
+            Sub
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void AdicionarNumero(string numero)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+            txtHistorico.Text.Contains("-") ||
+            txtHistorico.Text.Contains("x") ||
+            txtHistorico.Text.Contains("/"))
+            {
+                valores += numero;
+                txtHistorico.Text += numero;
+            }
+
+            else
+            {
+                if (txtHistorico.Text.Contains(Convert.ToString(Resultado)))
+                {
+                    valores = "";
+                    txtHistorico.Text = "";
+                    Resultado = -1m;
+                    Valor = 0;
+                    valores += numero;
+                    txtHistorico.Text += numero;
+                }
+
+                else
+                {
+                    valores += numero;
+                    txtHistorico.Text += numero;
+                }
+            }
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("0");
+        }
+        private void btnUm_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("1");
+        }
+        private void btnDois_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("2");
+        }
+        private void btnTres_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("3");
+        }
+        private void btnQuatro_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("4");
+        }
+        private void btnCinco_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("5");
+        }
+        private void btnSeis_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("6");
+        }
+        private void btnSete_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("7");
+        }
+        private void btnOito_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("8");
+        }
+        private void btnNove_Click(object sender, EventArgs e)
+        {
+            AdicionarNumero("9");
+        }
+
+        private void btnVirgula_Click(object sender, EventArgs e)
+        {
+            if (valores.Contains(","))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(txtHistorico.Text))
+                {
+                    valores += ",";
+                    txtHistorico.Text += ",";
+                    Resultado = -1m;
+                }
+            }
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            if (sinais.Contains("+") ||
+                 sinais.Contains("-") ||
+                 sinais.Contains("x") ||
+                 sinais.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(txtHistorico.Text))
+                {
+                    OperacaoSelecionada = Operacao.Div;
+                    Valor = Convert.ToDecimal(valores);
+                    valores = "";
+                    txtHistorico.Text += "/";
+                    sinais = "/";
+                }
+            }
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            if (sinais.Contains("+") ||
+                 sinais.Contains("-") ||
+                 sinais.Contains("x") ||
+                 sinais.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(txtHistorico.Text))
+                {
+                    OperacaoSelecionada = Operacao.Mult;
+                    Valor = Convert.ToDecimal(valores);
+                    valores = "";
+                    txtHistorico.Text += "x";
+                    sinais = "x";
+                }
+            }
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            if (sinais.Contains("+") ||
+                 sinais.Contains("-") ||
+                 sinais.Contains("x") ||
+                 sinais.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(txtHistorico.Text))
+                {
+                    OperacaoSelecionada = Operacao.Soma;
+                    Valor = Convert.ToDecimal(valores);
+                    valores = "";
+                    txtHistorico.Text += "+";
+                    sinais = "+";
+                }
+            }
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtHistorico.Text))
+            {
+                valores += "-";
+                txtHistorico.Text += "-";
+                sinais = "";
+            }
+
+            else
+            {
+
+                if (sinais.Contains("+") ||
+                 sinais.Contains("-") ||
+                 sinais.Contains("x") ||
+                 sinais.Contains("/"))
+                {
+                    valores += "";
+                    txtHistorico.Text += "";
+                }
+
+                else
+                {
+                    OperacaoSelecionada = Operacao.Sub;
+                    Valor = Convert.ToDecimal(valores);
+                    valores = "";
+                    txtHistorico.Text += "-";
+                    sinais = "-";
+                }
+            }
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Div:
+                    Resultado = Valor / Convert.ToDecimal(valores);
+                    break;
+
+                case Operacao.Mult:
+                    Resultado = Valor * Convert.ToDecimal(valores);
+                    break;
+
+                case Operacao.Soma:
+                    Resultado = Valor + Convert.ToDecimal(valores);
+                    break;
+
+                case Operacao.Sub:
+                    Resultado = Valor - Convert.ToDecimal(valores);
+                    break;
+
+            }
+            valores = Convert.ToString(Resultado);
+            txtHistorico.Text = Convert.ToString(Resultado);
+            sinais = "";
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            valores = "";
+            txtHistorico.Text = "";
+            sinais = "";
+            Valor = 0;
+        }
+
+        private void btnLimparUm_Click(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(txtHistorico.Text))
+            {
+                if (char.IsDigit(txtHistorico.Text[txtHistorico.Text.Length - 1]) || txtHistorico.Text[txtHistorico.Text.Length - 1] == ',')
+                {
+                    valores = valores.Substring(0, valores.Length - 1);
+                    txtHistorico.Text = txtHistorico.Text.Substring(0, txtHistorico.Text.Length - 1);
+                }
+            }
+            else
+            {
+                valores = "";
+                txtHistorico.Text = "";
+                Valor = 0;
+            }
+        }
+
+        private void btnBinario_Click(object sender, EventArgs e)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+            txtHistorico.Text.Contains("-") ||
+            txtHistorico.Text.Contains("x") ||
+            txtHistorico.Text.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(valores))
+                {
+                    int num = Convert.ToInt32(valores);
+                    valores = Convert.ToString(num, 2);
+                    txtHistorico.Text = valores;
+                    Resultado = Convert.ToDecimal(valores);
+                }
+            }
+        }
+
+        private void btnFatorial_Click(object sender, EventArgs e)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+          txtHistorico.Text.Contains("-") ||
+          txtHistorico.Text.Contains("x") ||
+          txtHistorico.Text.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(valores))
+                {
+                    int num = Convert.ToInt32(valores);
+                    Resultado = 1;
+                    for (int i = num; i > 1; i--)
+                    {
+                        Resultado *= i;
+                    }
+
+                    valores = Convert.ToString(Resultado);
+                    txtHistorico.Text = valores;
+                }
+            }
+        }
+
+        private void btnSen_Click(object sender, EventArgs e)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+          txtHistorico.Text.Contains("-") ||
+          txtHistorico.Text.Contains("x") ||
+          txtHistorico.Text.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(valores))
+                {
+                    double graus = Convert.ToDouble(valores);
+                    double radianos = graus * (Math.PI / 180); // Converte para radianos
+                    Resultado = Convert.ToDecimal(Math.Sin(radianos));
+
+                    valores = Convert.ToString(Resultado);
+                    txtHistorico.Text = valores;
+                }
+            }
+        }
+
+        private void btnCos_Click(object sender, EventArgs e)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+          txtHistorico.Text.Contains("-") ||
+          txtHistorico.Text.Contains("x") ||
+          txtHistorico.Text.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(valores))
+                {
+                    double graus = Convert.ToDouble(valores);
+                    double radianos = graus * (Math.PI / 180); // Converte para radianos
+                    Resultado = Convert.ToDecimal(Math.Cos(radianos));
+
+                    valores = Convert.ToString(Resultado);
+                    txtHistorico.Text = valores;
+                }
+            }
+        }
+
+        private void btnTan_Click(object sender, EventArgs e)
+        {
+            if (txtHistorico.Text.Contains("+") ||
+          txtHistorico.Text.Contains("-") ||
+          txtHistorico.Text.Contains("x") ||
+          txtHistorico.Text.Contains("/"))
+            {
+                valores += "";
+                txtHistorico.Text += "";
+            }
+
+            else
+            {
+                if (!string.IsNullOrEmpty(valores))
+                {
+                    double graus = Convert.ToDouble(valores);
+                    double radianos = graus * (Math.PI / 180); // Converte para radianos
+                    Resultado = Convert.ToDecimal(Math.Tan(radianos));
+
+                    valores = Convert.ToString(Resultado);
+                    txtHistorico.Text = valores;
+                }
+            }
+        }
+    }
+}
